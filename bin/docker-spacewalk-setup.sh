faae4681c087dbb871963f5046fec0c3a7bb4db4
Pavel Studenik <pstudeni@redhat.com>
2017-05-05 14:34:42 +0200

    anable  upgrade systems

--- bin/docker-spacewalk-setup.sh
+++ bin/docker-spacewalk-setup.sh
@@ -12,6 +12,16 @@ function query() {
     psql -h $DOCKER_POSTGRESQL -U postgres <<< "$1"
 }
 
+function schema {
+     spacewalk-sql -i <<EOF
+        select rhnPackageName.name || '-' || (PE.evr).version || '-' || (PE.evr).release
+        from rhnVersionInfo, rhnPackageName, rhnPackageEVR PE
+        where rhnVersionInfo.label = 'schema'
+                and rhnVersionInfo.name_id = rhnPackageName.id
+                and rhnVersionInfo.evr_id = PE.id;
+EOF
+}
+
 query "select version()" || exit 1
 query "CREATE USER $DB_USER with password '$DB_PASS';"
 query "CREATE DATABASE $DB_NAME;"
@@ -24,7 +34,12 @@ createlang pltclu $DB_NAME -h $DOCKER_POSTGRESQL -U postgres
 sed -i 's/\(^\s*wait_for_tomcat\)/#\1/g' /usr/bin/spacewalk-setup
 sed '3i\echo "Docker workaround - skip restarting..." && exit 0\' -i /usr/sbin/spacewalk-service
 
-spacewalk-setup --external-postgresql --answer-file=/root/answer.txt --clear-db --skip-services-restart
+if schema | grep spacewalk-schema; then
+    spacewalk-setup --external-postgresql --answer-file=/root/answer.txt --upgrade --skip-services-restart --non-interactive
+    spacewalk-schema-upgrade
+else
+    spacewalk-setup --external-postgresql --answer-file=/root/answer.txt --clear-db --skip-services-restart --non-interactive
+fi
 
 [ -z "$HOST_HOSTNAME" ] || /root/spacewalk-hostname-rename.sh $HOST_HOSTNAME
 
